86c7b5524898cc5755e4146a5107cfa83bb3befa
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2021-11-30 09:21:41 -0300

    Adicionando outra forma de fazer o exercicio de frase do bart

--- Exercicios_Javascript/FraseBartOutraForma/script.js
+++ Exercicios_Javascript/FraseBartOutraForma/script.js
@@ -6,6 +6,8 @@ const sectionResult = document.querySelector('.sectionResult')
 submit.addEventListener('click',stopDefAction)
 submit.addEventListener('click',changeScreens);
 let timeouts = [];
+let contDelete=0;
+let numLines=0;
 backButton.addEventListener('click',backScreen);
 
 function stopDefAction(evt) {
@@ -19,7 +21,7 @@ function backScreen(){
     form.style.display = 'flex';
     backButton.style.display = "none";
     main.style.display ="none";
-    main.innerHTML = '';
+    main.innerHTML = '<p></p>';
     sectionResult.style.display = "none"
 
     
@@ -33,26 +35,20 @@ function changeScreens(){
     sectionResult.style.display = "none"
     write(num);
 }
-function createElementPhrase(cont){
-    let a = document.createElement('p')
-    a.setAttribute('id',"phrase-"+cont);
-    return a;
-}
 function choosePhrase(num){
     const PHRASE = [
-        "Alguma criança ainda faz isso?",
-        "Eu não sou mais esperto que o presidente",
-        "A professora não levou um fora, foi mutuo",
-        "Não estamos nus debaixo das roupas",
-        "Pérolas não são vômitos de ostra",
-        "Cerveja numa caixa de leite não é leite"
+        "Alguma criança ainda faz isso?     ",
+        "Eu não sou mais esperto que o presidente ",
+        "A professora não levou um fora, foi mutuo ",
+        "Não estamos nus debaixo das roupas     ",
+        "Pérolas não são vômitos de ostra       ",
+        "Cerveja numa caixa de leite não é leite "
     ];
     let phrase = PHRASE[num];
-    //return phrase;
-    let a = "A professora não levou um fora, foi mutuo"
-    return a;
+    phrase ="A professora não levou um fora, foi mutuo      ";
+    return phrase;
 }
-function typeWriterPhrase(cont,PHRASE){
+function typeWriterPhrase(PHRASE){
     
     let i = 0;
     let txt = PHRASE;
@@ -60,18 +56,22 @@ function typeWriterPhrase(cont,PHRASE){
     typeWriter();
     function typeWriter(){
     if (i < txt.length) {
-        document.querySelector('#phrase-'+cont).innerHTML += txt.charAt(i);
+        document.querySelector("main >p").innerHTML += txt.charAt(i);
         i++;
-        console.log('asd')
         setTimeout(typeWriter, speed);
+        if(countLines() >=11){
+            clearScreen(PHRASE)
+        }
     }
 }
-
+}
+function clearScreen(PHRASE){
+    main.innerHTML = '<p></p>';
+    numLines=1;
+    contDelete++
 }
 function write(num){
     let cont=1;
-    let id=0;
-    let contDelete=0;
     const numPhrase = Math.floor(Math.random() * 6);
     if(num <=0 ){
         alert('Digite um número positivo!')
@@ -79,25 +79,10 @@ function write(num){
     }
     else{
         while(cont <= num){
-            
-            if(cont%11 == 1 && cont!=1){
-                timeouts.push(setTimeout(() => {
-                main.innerHTML = '';
-                main.append(createElementPhrase(id));
-                typeWriterPhrase(id,choosePhrase(numPhrase));
-                console.log(createElementPhrase(id))
-                id++;
-               }, 2100 *cont));
-
-               contDelete++
-            }else{
-                timeouts.push(setTimeout(() => {
-                main.append(createElementPhrase(id));
-                typeWriterPhrase(id,choosePhrase(numPhrase));
-                console.log(createElementPhrase(id))
-                id++
-                }, 2100*cont));
-            }
+            timeouts.push(setTimeout(() => {
+                typeWriterPhrase(choosePhrase(numPhrase));
+                numLines++;
+            }, 2700 *cont));
             cont++;
         }
         
@@ -113,7 +98,16 @@ function write(num){
             
             const title2 = document.querySelector('section > h2')
             title2.textContent = 'A quantidade de linhas na tela final foi '
-            title2.textContent += (cont-1)%11 == 0 ? 11 : (cont-1)%11;            
-            }, 2200*cont));
+            title2.textContent += numLines;            
+            }, 2600*cont));
     }
+}
+
+
+function countLines() {
+    let el = document.querySelector("main > p");
+    let divHeight = el.offsetHeight
+    let lineHeight = 30;
+    let lines = divHeight / lineHeight;
+    return lines
 }
