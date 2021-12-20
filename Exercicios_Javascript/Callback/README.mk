a3793e7c7ac9783ad72ac04fb4aceae31f7255f7
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2021-12-20 18:21:33 -0300

    Adicionando Exercicios de Função callback

---
+++ Exercicios_Javascript/Callback/README.mk
@@ -0,0 +1,7 @@
+Utilize uma callback function para executar o sorteio e setInterval() com 1 segundo de diferença entre cada número sorteado.
+
+A mega-sena possui números de 1 a 60;
+É sorteado um número por vez;
+Nenhum número pode se repetir no sorteio;
+São sorteados 6 números ao todo.
+O uso do método setInterval() é obrigatório.
