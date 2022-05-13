c0bace11712876449d2efc81cbe2882cf1c6f44a
Vitor Ramos <vitorrt2015@gmail.com>
2022-05-13 14:01:52 -0300

    Delete ArquivosExercicios directory

--- ArquivosExercicios/JavaScript/postdelete/exercicio/backend/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config
-const config = require('./src/config/index')
-
-const app = require('./src/app')
-
-const port = config.app.port
-app.listen(port,()=>console.log(`Ouvindo na porta ${port} ...`))
--- ArquivosExercicios/JavaScript/postdelete/exercicio/frontend/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app =  express()
-
-app.use(express.static('./src'))
-const port = 3005
-app.listen(port,()=>console.log(`Ouvindo na porta ${port} ...`))
